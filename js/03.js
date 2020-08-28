bd4e9bc03ec87e506bee0458e40e047499a06f86
elenapanasyuk <elena.panasyuk@gmail.com>
2020-08-28 23:32:50 +0300

    Решение 1-5

--- js/03.js
+++ js/03.js
@@ -1,71 +1,120 @@
-/*использование методов класса
-Напиши класс Storage, который будет создавать объекты для управления складом товаров.
-При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+//Задача 6-3
+const users = [
+  {
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '249b6175-5c30-44c6-b154-f120923736f5',
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
+];
+/*
+filter, map
+Получи массив имен пользователей(значение свойства name) по полу(значение свойства gender).
 
-Добавь методы класса:
+Используй деструктурирующее присваивание для параметра функции({ name }) без пробелов и переносов на новую строку.
 
-getItems() - возвращает массив текущих товаров
-addItem(item) - получает новый товар и добавляет его к текущим
-removeItem(item) - получает товар и, если он есть, удаляет его из текущих
+Используй только перебирающие методы массива которые не изменяют(не мутируют) исходный массив.
+Т.е.нельзя использовать for, splice, push и т.п.мутирующие методы.
 */
 
 // Write code under this line
+const getUsersWithGender = (array, gender) =>
+  array.filter(user => user.gender === gender).map(({ name }) => name);
 
-class Storage {
-  constructor(items) {
-    this.items = items;
-  }
+//const getUsersWithEyeColor = (array, color) => array.filter(({ eyeColor }) => eyeColor === color);
+//const getUserNames = array => array.map(({ name }) => name);
 
-  getItems() {
-    return this.items;
-  }
-  //- возвращает массив текущих товаров
+/*
+const result = numbers
+  .filter(x => x % 2 === 0)
+  .map(y => y * 2)
+  .reverse();
 
-  //addItem(item)
-  addItem(item) {
-    this.items.push(item);
-  }
-  removeItem(item) {
-    for (let i = 0; i < this.items.length; i += 1) {
-      if (item === this.items[i]) {
-        this.items.splice(i, 1);
-      }
-    }
-  }
-}
+console.log(result);
+*/
+console.log(getUsersWithGender(users, 'male'));
 
-// console.log(typeof Storage);
-// 'function'
-
-const goods = ['Нанитоиды', 'Пролонгер', 'Железные жупи', 'Антигравитатор'];
-
-const storage = new Storage(goods);
-
-console.log(storage.getItems());
 /* [
-  'Нанитоиды',
-  'Пролонгер',
-  'Железные жупи',
-  'Антигравитатор'
+  "Moore Hensley",
+  "Ross Vazquez",
+  "Carey Barr",
+  "Blackburn Dotson"
 ] */
-
-storage.addItem('Дроид');
-console.log(storage.getItems());
-console.log(goods.length);
-/* [
-  'Нанитоиды',
-  'Пролонгер',
-  'Железные жупи',
-  'Антигравитатор',
-  'Дроид'
-] */
-
-storage.removeItem('Пролонгер');
-console.log(storage.getItems());
-/* [
-  'Нанитоиды',
-  'Железные жупи',
-  'Антигравитатор',
-  'Дроид'
-] */
-console.log(goods.length);
